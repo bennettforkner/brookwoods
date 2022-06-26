031ed698aead12e152226d64e96760e6f50efac5
Bennett F Forkner <43764277+bennettforkner@users.noreply.github.com>
2022-06-25 21:19:19 -0400

    Remove test message

--- api/downloadActivitySignupsList.php
+++ api/downloadActivitySignupsList.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-echo "hello";
 // Fetch records from database
 $sql = "CALL `Get Activity Signups List`(
 	" . $mysqli->real_escape_string($_GET['year']) . ",
